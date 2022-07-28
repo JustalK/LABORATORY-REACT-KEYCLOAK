9547e6a4dfcbe08b6152e426bcd53537a7bea641
justalk <justal.kevin@gmail.com>
2022-07-28 06:09:50 +0200

    [FEATURE] Making keyloak working on react app1

--- project/apps/app1/src/app/app.tsx
+++ project/apps/app1/src/app/app.tsx
@@ -1,14 +1,26 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
+import React from "react";
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import keycloak from "./Keycloak"
+import PrivateRoute from "./helpers/PrivateRoute";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import WelcomePage from "./pages/Homepage";
+import SecuredPage from "./pages/Securedpage";
 
-export function App() {
-  return (
-    <>
-      <NxWelcome title="app1" />
-      <div />
-    </>
-  );
+function App() {
+ return (
+   <div>
+   <ReactKeycloakProvider authClient={keycloak}>
+       <Nav />
+       <BrowserRouter>
+         <Routes>
+           <Route path="/" element={<WelcomePage />} />
+           <Route path="/secured" element={<PrivateRoute><SecuredPage /></PrivateRoute>} />
+         </Routes>
+       </BrowserRouter>
+     </ReactKeycloakProvider>
+   </div>
+ );
 }
 
 export default App;
